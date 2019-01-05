e1b808855b7e95c3fac9d743e9699d9af40ad852
przemekkruczek <przemyslawkruczek.1990@gmail.com>
2019-01-05 01:23:12 +0100

    rwd commit

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -4,7 +4,7 @@ export class Header extends React.Component {
     render() {
         return (
             <header className='header'>
-                <a href="/" className='header-logo'><img src='/images/logo.png' alt='InfoMovie' /><h1 className='header-logo__title'>InfoMovie</h1></a>
+                <a href="https://przemekkruczek.github.io/InfoMovie/#/" className='header-logo'><img src='/images/logo.png' alt='InfoMovie' /><h1 className='header-logo__title'>InfoMovie</h1></a>
                 <a href="https://github.com/przemekkruczek" className='header-sign'><h3>Made by Przemyslaw Kruczek</h3></a>
             </header>
         )
